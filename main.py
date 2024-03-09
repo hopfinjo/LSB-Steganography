7383086cf26320851ec5f9e28abfee638e2111c7
Maxi Hopfer <maxi.hopfer@gmx.at>
2024-03-08 21:23:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,13 +19,14 @@ with Image.open('./test-images/test-images/original-test-images/1by5.bmp') as im
     print("\n")
     print(bits)
     print("\n")
-    # print("And it has ",len(bits) , " length")
-    # if (len(bits)/8) > 8:
-    #     print("YOU CAN STORE ONE INTEGER IN THIS PICTURES")
-    # else:
-    #     print("YOU CAN STORE AT LEAST ONE ASCI NR IN IT")
+    print("And it has ",len(bits) , " length")
 
-    # print("that means there are bytes (packs of 8 bits)", len(bits)/8)
+    if (len(bits)/8) > 8:
+        print("YOU CAN STORE ONE INTEGER IN THIS PICTURES")
+    else:
+        print("YOU CAN NOT NOT NOT IT HAS LESS THAN 8 BYTES STORE AT LEAST ONE ASCI NR IN IT")
+
+    print("that means there are bytes (packs of 8 bits)", len(bits)/8)
     print("\n")
     print() 
 
